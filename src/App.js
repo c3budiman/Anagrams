9944e8adacd7f2389cdc52d503f87bb08b8cf5b3
Cecep Budiman <c3budiman@gmail.com>
2021-02-15 22:34:48 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { isAnagram } from './helpers/anagramsolverbasic'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [word, setWord] = useState("");
+
+  const [listWords, setlistWords] = useState([]);
+  
+  const [anagramList, setAnagramList] = useState([]);
+
+	const AddWords = () => {
+    let curListWords = listWords;
+		if(!curListWords.includes(word)) {
+      curListWords.push(word);
+      setlistWords(curListWords);
+      try {
+        setAnagramList(isAnagram(curListWords))
+      } catch (error) {
+        alert(error) 
+      }
+    }
+		setWord("");
+		document.getElementById("words").focus();
+	}
+
+
+	return (
+		<div>
+			<div className="Header">
+				<img alt="Logo" className="Logo" height="80px" src="/wb.JPG" />
+				<h1 className="HeaderText">Anagram checker</h1>
+			</div>
+
+			<div className="flex-container">
+				<div className="flex-input">
+					<p className="HeadingWords">
+						Add any words here :
+					</p>
+          <input 
+            value={word} 
+            placeholder="kata"
+            onChange={(e) => setWord(e.target.value)} 
+            onKeyPress={e => {
+              if (e.key === 'Enter') {
+                AddWords()
+              }
+            }}
+            id="words" type="text">
+          </input> 
+          <br />
+					<button id="addWords" className="btn btn-green" onClick={() => AddWords()}>Add</button>
+				</div>
+
+				<div className="flex-input-temp">
+					<p className="HeadingWords">
+						Added Words :
+					</p>
+					<ul className="addedWords">
+					{
+						listWords.map(element => {
+							return (<li key={element}>{element}</li>)
+						})
+					}
+					</ul>
+				</div>
+			</div>
+
+			<div className="flex-container">
+        <div className="flex-result">
+          <p className="HeadingWords">
+						Anagram List :
+					</p>
+          <p id="anagramJson">
+            {
+              JSON.stringify(anagramList)
+            }
+          </p>
+        </div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, queryByAttribute } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
+
+test('renders page without crash', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Anagram checker/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test('type some words, click add, and calculate the anagram', () => {
+  const dom = render(<App />);
+  const getById = queryByAttribute.bind(null, 'id');
+  const wordsInput = getById(dom.container, 'words');
+  const ButtonAdd = getById(dom.container, 'addWords');
+  const anagramJson = getById(dom.container, 'anagramJson');
+
+  userEvent.type(wordsInput, 'kata');
+  userEvent.click(ButtonAdd);
+
+  userEvent.type(wordsInput, 'taka');
+  userEvent.click(ButtonAdd);
+
+  userEvent.type(wordsInput, 'aw');
+  userEvent.click(ButtonAdd);
+
+  expect(anagramJson).toHaveTextContent('[["kata","taka"],["aw"]]')
 });
